bba77c33f1db7e70115385f5229465389a1b482f
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2021-02-10 14:26:25 +0300

    Add and remove from cart with redundacy; no api

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
-var updateBtns =document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i=0; i<updateBtns.length; i++){
+for (var i=0; i<updateBtns.length; i++){
     updateBtns[i].addEventListener('click' , function(){
         var productId = this.dataset.product
         var action = this.dataset.action
@@ -24,13 +24,16 @@ function updateUserOrder(productId , action){
         method:'POST',
         headers:{
             'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
         },
         body:JSON.stringify({'productId':productId , 'action':action})
     })
     .then((response) =>{
-        return response.json();
+        return response.json()
     })
     .then((data) =>{
-        console.log('Data:',data)
+        console.log('data:',data)
+        location.reload()
     });
+
 }
